90a5441868fae4f5a692ccf5527d0b4774fa601d
DominicDev <s0l0estudi4r@gmail.com>
2025-05-10 11:51:31 +0200

    v0.0.2

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,1 +1,2 @@
-export const URL = 'http://localhost:5173/data.json';
+// export const URL = 'http://localhost:5173/data.json';
+export const URL = 'https://20essentials.github.io/project-000-947/data.json';
